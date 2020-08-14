63a170b383e51fb928307ce58920af7c22cb4167
reud <reiji.kobayashi.9n@stu.hosei.ac.jp>
2020-08-14 15:29:20 +0900

    fix typo

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 import Home from "../views/Home.vue";
+import Test from "../views/Test.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes: Array<RouteConfig> = [
     path: "/",
     name: "Home",
     component: Home
+  },
+  {
+    path: "/test",
+    name: "Test",
+    component: Test
   },
   {
     path: "/about",
